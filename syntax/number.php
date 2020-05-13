0857b196dc1b517c0828a9142452f36ac79e3307
AaronLab <aaronlab.net@gmail.com>
2020-05-13 09:15:49 +0900

    Create number.php

---
+++ syntax/number.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>number</title>
+</head>
+<body>
+    <h1>Number & Arithmetic Operator</h1>
+    <h2>1+1</h2>
+    <?php
+    echo 1+1;
+    ?>
+    <h2>2-1</h2>
+    <?php
+    echo 2-1;
+    ?>
+    <h2>2*2</h2>
+    <?php
+    echo 2*2;
+    ?>
+    <h2>4/2</h2>
+    <?php
+    echo 4/2;
+    ?>
+</body>
+</html>
