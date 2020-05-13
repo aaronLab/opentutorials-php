81dc34cac7e10b5c61c2666199efdf5cdfe1d849
AaronLab <aaronlab.net@gmail.com>
2020-05-13 19:11:25 +0900

    Create comparison.php

---
+++ syntax/comparison.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Comparison</title>
+</head>
+<body>
+    <h1>Comparison Operators &amp; Boolean Data Type</h1>
+    <h2>1==1</h2>
+    <?php
+    var_dump(1==2);
+    ?>
+    <h2>1>1</h2>
+    <?php
+    var_dump(1>1);
+    ?>
+    <h2>1>=1</h2>
+    <?php
+    var_dump(1>=1);
+    ?>
+</body>
+</html>
