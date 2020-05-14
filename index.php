8f5f3a403553264c7ba8aa645e31775ad3ae7baf
Aaron Lee <63542621+aaronLab@users.noreply.github.com>
2020-05-14 23:14:30 +0900

    Update index.php

--- index.php
+++ index.php
@@ -7,10 +7,25 @@
   <body>
     <h1><a href="index.php">WEB</a></h1>
     <ol>
+    <?php
+      $list = scandir('data');
+      $i = 0;
+      while(i < count($list)){
+        if($list[$i] != '.'){
+          if($list[$i] != '..'){
+            echo "<li><a href=\"index.php?id=$list[$i]\">$list[$i]</a></li>\n";
+          }
+        }
+        $i = $i + 1;
+      }
+    ?>
+    </ol>
+    <!-- <ol>
       <li><a href="./index.php?id=HTML">HTML</a></li>
       <li><a href="./index.php?id=CSS">CSS</a></li>
       <li><a href="./index.php?id=JavaScript">JavaScript</a></li>
-    </ol>
+      <li><a href="./index.php?id=PHP">PHP</a></li>
+    </ol> -->
     <h2>
         <?php
         if(isset($_GET['id'])){
