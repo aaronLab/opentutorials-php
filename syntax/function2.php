650974c89f19d15f7b841eaccc90de64f6d29be1
Aaron Lee <63542621+aaronLab@users.noreply.github.com>
2020-05-15 21:20:21 +0900

    Update function2.php

--- syntax/function2.php
+++ syntax/function2.php
@@ -13,9 +13,26 @@
         print("Lorem 1<br>");
         print("Lorem 2<br>");
     }
-
     basic();
     basic();
+    basic();
+    ?>
+    <h2>Parameter &amp; Argument</h2>
+    <?php
+    function sum($left, $right){
+        print($left+$right);
+        print("<br>");
+    }
+    sum(2, 4);
+    sum(4, 6);
+    ?>
+    <h2>Return</h2>
+    <?php
+    function sum2($left, $right){
+        return $left+$right;
+    }
+    print(sum(2, 4));
+    file_put_contents('result.txt', sum2(2, 4));
     ?>
 </body>
 </html>
