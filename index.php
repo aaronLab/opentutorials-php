adba5d6095689e7a1331ba39288ae3c664f14b2d
Aaron Lee <63542621+aaronLab@users.noreply.github.com>
2020-05-15 23:39:02 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,54 +1,7 @@
 <?php
-function print_title(){
-  if(isset($_GET['id'])){
-    echo $_GET['id'];
-  } else {
-    echo 'WEB';
-  }
-}
-
-function print_description(){
-  if(isset($_GET['id'])){
-    echo file_get_contents("./data/".$_GET['id']);
-  } else{
-    echo file_get_contents("./data/WEB");
-  }
-}
-
-function print_list(){
-  $list = scandir("./data");
-  $i = 0;
-  while ($i < count($list)) {
-    if ($list[$i] != ".") {
-      if ($list[$i] != "..") {
-        if ($list[$i] != "WEB") {
-          echo "<li><a href=\"./index.php?id=$list[$i]\">$list[$i]</a></li>";
-        }
-      }
-    }
-    $i += 1;
-  }
-}
+require_once('./lib/print.php');
+require('./view/top.php');
 ?>
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>
-    <?php
-        print_title();
-        ?>
-        </title>
-    <meta charset="utf-8" />
-  </head>
-  <body>
-    <h1><a href="index.php">WEB</a></h1>
-    <ol>
-    <?php
-    print_list();
-    ?>
-    </ol>
-    <a href="./create.php">Create</a>
-    
     <?php if(isset($_GET['id'])){?>
       <a href="./modify.php?id=<?=$_GET['id'] ?>">Modify</a>
       <form action="./delete_process.php" method="post">
@@ -66,5 +19,6 @@ function print_list(){
       print_description();
       ?>
     </p>
-  </body>
-</html>
+<?php
+require('./view/bottom.php');
+?>
