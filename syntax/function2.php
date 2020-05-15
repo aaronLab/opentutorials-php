bd38d25a77c2307953b51ea16cc3925a24729095
Aaron Lee <63542621+aaronLab@users.noreply.github.com>
2020-05-15 17:02:20 +0900

    Create function2.php

---
+++ syntax/function2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>function</title>
+</head>
+<body>
+    <h1>Function</h1>
+    <h2>Basic</h2>
+    <?php
+    function basic(){
+        print("Lorem 1<br>");
+        print("Lorem 2<br>");
+    }
+
+    basic();
+    basic();
+    ?>
+</body>
+</html>
