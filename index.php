abb11a047f6b487bd1555a2eafc9cef8863581d5
Aaron Lee <63542621+aaronLab@users.noreply.github.com>
2020-05-15 21:56:43 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,43 +1,60 @@
+<?php
+function print_title(){
+  if(isset($_GET['id'])){
+    echo $_GET['id'];
+  } else {
+    echo 'WEB';
+  }
+}
+
+function print_description(){
+  if(isset($_GET['id'])){
+    echo file_get_contents("./data/".$_GET['id']);
+  } else{
+    echo file_get_contents("./data/WEB");
+  }
+}
+
+function print_list(){
+  $list = scandir("./data");
+  $i = 0;
+  while ($i < count($list)) {
+    if ($list[$i] != ".") {
+      if ($list[$i] != "..") {
+        if ($list[$i] != "WEB") {
+          echo "<li><a href=\"./index.php?id=$list[$i]\">$list[$i]</a></li>";
+        }
+      }
+    }
+    $i += 1;
+  }
+}
+?>
 <!DOCTYPE html>
 <html>
   <head>
-    <title>WEB1 - Welcome</title>
+    <title>
+    <?php
+        print_title();
+        ?>
+        </title>
     <meta charset="utf-8" />
   </head>
   <body>
     <h1><a href="index.php">WEB</a></h1>
     <ol>
     <?php
-      $list = scandir("./data");
-      $i = 0;
-      while ($i < count($list)) {
-        if ($list[$i] != ".") {
-          if ($list[$i] != "..") {
-            if ($list[$i] != "WEB") {
-              echo "<li><a href=\"./index.php?id=$list[$i]\">$list[$i]</a></li>";
-            }
-          }
-        }
-        $i += 1;
-      }
+    print_list();
     ?>
     </ol>
     <h2>
         <?php
-        if(isset($_GET['id'])){
-          echo $_GET['id'];
-        } else {
-          echo 'WEB';
-        }
+        print_title();
         ?>
     </h2>
     <p>
       <?php
-      if(isset($_GET['id'])){
-        echo file_get_contents("./data/".$_GET['id']);
-      } else{
-        echo file_get_contents("./data/WEB");
-      }
+      print_description();
       ?>
     </p>
   </body>
