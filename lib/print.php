a218617c28497d644532bd072093b287b1572caf
Aaron Lee <63542621+aaronLab@users.noreply.github.com>
2020-05-15 23:50:23 +0900

    Update print.php

--- lib/print.php
+++ lib/print.php
@@ -1,7 +1,7 @@
 <?php
 function print_title(){
   if(isset($_GET['id'])){
-    echo $_GET['id'];
+    echo htmlspecialchars($_GET['id']);
   } else {
     echo 'WEB';
   }
@@ -9,7 +9,7 @@ function print_title(){
 
 function print_description(){
   if(isset($_GET['id'])){
-    echo file_get_contents("./data/".$_GET['id']);
+    echo htmlspecialchars(file_get_contents("./data/".$_GET['id']));
   } else{
     echo file_get_contents("./data/WEB");
   }
@@ -19,10 +19,11 @@ function print_list(){
   $list = scandir("./data");
   $i = 0;
   while ($i < count($list)) {
+    $title = htmlspecialchars($list[$i]);
     if ($list[$i] != ".") {
       if ($list[$i] != "..") {
         if ($list[$i] != "WEB") {
-          echo "<li><a href=\"./index.php?id=$list[$i]\">$list[$i]</a></li>";
+          echo "<li><a href=\"./index.php?id=$title\">$title</a></li>";
         }
       }
     }
